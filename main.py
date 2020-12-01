1b20a74a338f9eb41f0330b034182497403f616e
matetzky <66177370+matetzky@users.noreply.github.com>
2020-12-01 18:43:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ class GuyMagen():
     self.penisSize = 0
     self.favoriteRabi = 'kanievski'
     
+  def __repr__(self):
+    print('<=====3')
     
+  def didGuyKillRabin(self):
+    return True 
+  
+guyMagen = 0
+
+import matplotlib.pyploy as plt 
+x = [i for i in range(guyMagen,20)]
+y = [a**2 fro a in x]
+plt.plot(x,y)
+
